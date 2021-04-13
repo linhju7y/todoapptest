9d5a9f9518653dd9404697598ea64ec60a6e064e
DESKTOP-OBQC0DF\HUNG PC <linh.mhl9@gmail.com>
2021-04-14 00:05:53 +0700

    Updates

---
+++ precache-manifest.6a50fc3c1d05a757fcf2925f0f49bc2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4386c089ba2fc9cae53b443979e8bd1",
+    "url": "/todoapptest/index.html"
+  },
+  {
+    "revision": "2c1bc57a643a5c1baa8b",
+    "url": "/todoapptest/static/js/2.13d4bdb4.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/todoapptest/static/js/2.13d4bdb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ffdc6e8eb41963cc007",
+    "url": "/todoapptest/static/js/main.c7c9308e.chunk.js"
+  },
+  {
+    "revision": "4e379b5a5b699268b3a8",
+    "url": "/todoapptest/static/js/runtime-main.d2a7f434.js"
+  }
+]);
